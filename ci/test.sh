715813bb49aeab73346506916e2b7c2d6ff805f0
Dhole <dhole@riseup.net>
2024-05-04 20:01:41 +0200

    update: arduboy deps

--- ci/test.sh
+++ ci/test.sh
@@ -5,34 +5,35 @@ ROOT=${ROOT:-$(git rev-parse --show-toplevel)}
 export PATH=$HOME/.cargo/bin:$PATH
 
 if [ $# -lt 1 ]; then
-    echo "Usage: $0 TESTNAME [FEATURES]"
+    echo "Usage: $0 CMD [FEATURES]"
     exit 1
 fi
 
-testname="$1"
+cmd="$1"
 features="$2"
+arg="$3"
 
-case $testname in
+case $cmd in
     build)
         cd ${ROOT}/arduboy
         cargo build --target x86_64-unknown-linux-gnu --no-default-features --features "${features}"
         ;;
     unit)
         cd ${ROOT}/arduboy
-        cargo test --target x86_64-unknown-linux-gnu --no-default-features --features "${features}" $3
+        cargo test --target x86_64-unknown-linux-gnu --no-default-features --features "${features}" $arg
         ;;
     vector)
         cd ${ROOT}/test-framework
         tar -xf vectors.tar.xz
 
         cd ${ROOT}/arduboy
-        cargo test --target x86_64-unknown-linux-gnu --features test_vectors test_vectors $3
+        cargo test --target x86_64-unknown-linux-gnu --features test_vectors test_vectors $arg
 
         cd ${ROOT}/test-framework
         rm -r vectors
         ;;
     *)
-        echo "Unknown testname ${target}"
+        echo "Unknown cmd ${cmd}"
         exit 1
         ;;
 esac
